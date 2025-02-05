6502950774779e7d8024d49dfc0c0a19b916a9f8
SepVeneto <sepveneto@qq.com>
2025-02-05 14:53:10 +0800

    chore: remove clean

--- internal/build/gulpfile.ts
+++ internal/build/gulpfile.ts
@@ -44,9 +44,6 @@ export const copyFullStyle = async () => {
 }
 
 export default series(
-  withTaskName('clean', () => run('pnpm run clean')),
-  withTaskName('createOutput', () => mkdir(epOutput, { recursive: true })),
-
   parallel(
     runTask('buildModules'),
     runTask('buildFullBundle'),
